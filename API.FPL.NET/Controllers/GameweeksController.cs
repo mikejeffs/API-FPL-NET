c01d9eb857ffbb7c2ea4d28e5533919fcec41686
Mike Pratt <mikep2419@gmail.com>
2019-07-13 22:01:17 +0100

    Added gameweeks controller
    Added endpoints for getting a users history, and their picks for a game week.

---
+++ API.FPL.NET/Controllers/GameweeksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FPL.NET.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.FPL.NET.Controllers
+{
+    [Route("api/v0/game-week")]
+    [Produces("application/json")]
+    [ApiController]
+    public class GameweeksController : ControllerBase
+    {
+        private readonly GameweekService _gameweekService;
+
+        public GameweeksController(GameweekService service)
+        {
+            _gameweekService = service;
+        }
+
+    }
+}
--- API.FPL.NET/Controllers/UserEntriesController.cs
+++ API.FPL.NET/Controllers/UserEntriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FPL.NET.Models;
+using FPL.NET.Models.User;
 using FPL.NET.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -42,6 +43,47 @@ namespace API.FPL.NET.Controllers
 //                return NotFound("User not found or some error occurred");
             }
             return Ok(user);
+        }
+
+        [HttpGet("{id}/history")]
+        public IActionResult GetSeasonHistory(int id)
+        {
+            UserHistory seasonHistory = null;
+            string err = null;
+            _userEntryService.GetUserSeasonHistory(id).Result.Subscribe(result => { seasonHistory = result; },
+                (error) => { err = error.Message; },
+                () => { });
+
+            return Ok(seasonHistory);
+        }
+
+        [HttpGet("{id}/game-week/{eventId}/picks")]
+        public IActionResult GetPicksForEvent(int id, int eventId)
+        {
+            UserEventPicks picks = null;
+            string errorMessage = string.Empty;
+            _userEntryService.GetUserPlayerPicksForEvent(id, eventId).Result.Subscribe(res => picks = res,
+                error => errorMessage = error.Message,
+                () => { });
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                return BadRequest(errorMessage);
+            }
+
+            return Ok(picks);
+        }
+
+        [HttpGet("{id}/transfers")]
+        public IActionResult GetTransfers(int id)
+        {
+            throw new NotImplementedException("Endpoint not found, so cannot implement this yet!");
+        }
+
+        [HttpGet("{id}/cup")]
+        public IActionResult GetCups(int id)
+        {
+            throw new NotImplementedException("Endpoint not found, so cannot implement this yet!");
         }
     }
 }

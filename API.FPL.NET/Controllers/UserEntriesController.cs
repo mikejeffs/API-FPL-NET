0e8eebae8deb41cb2ecb50941136ac337ba78e42
Mike Pratt <mikep2419@gmail.com>
2019-09-22 17:24:15 +0100

    Made all endpoints in UserEntriesController asynchronous.

--- API.FPL.NET/Controllers/UserEntriesController.cs
+++ API.FPL.NET/Controllers/UserEntriesController.cs
@@ -25,13 +25,14 @@ namespace API.FPL.NET.Controllers
         [ProducesResponseType(typeof(User), 200)]
         [ProducesResponseType(400)]
         [Description("Returns a User from the specified id if one exists.")]
-        public IActionResult Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
             Exception err = null;
             User user = null;
-            _userEntryService.GetUserEntry(id).Result.Subscribe((result) =>
+            var result = await _userEntryService.GetUserEntry(id);
+            result.Subscribe((res) =>
             {
-                user = result;
+                user = res;
                 Console.WriteLine(user);
             },
             (error) =>
@@ -42,7 +43,6 @@ namespace API.FPL.NET.Controllers
             if (user == null)
             {
                 BadRequest(err.Message);
-//                return NotFound("User not found or some error occurred");
             }
             return Ok(user);
         }
@@ -51,13 +51,19 @@ namespace API.FPL.NET.Controllers
         [ProducesResponseType(typeof(UserHistory), 200)]
         [ProducesResponseType(400)]
         [Description("Returns a Users previous season data from the specified id if one exists.")]
-        public IActionResult GetSeasonHistory(int id)
+        public async Task<IActionResult> GetSeasonHistory(int id)
         {
             UserHistory seasonHistory = null;
-            string err = null;
-            _userEntryService.GetUserSeasonHistory(id).Result.Subscribe(result => { seasonHistory = result; },
-                (error) => { err = error.Message; },
+            string errorMessage = string.Empty;
+            var result = await _userEntryService.GetUserSeasonHistory(id);
+                result.Subscribe(res => { seasonHistory = res; },
+                (error) => { errorMessage = error.Message; },
                 () => { });
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                return BadRequest(errorMessage);
+            }
 
             return Ok(seasonHistory);
         }
@@ -66,11 +72,12 @@ namespace API.FPL.NET.Controllers
         [ProducesResponseType(typeof(UserEventPicks), 200)]
         [ProducesResponseType(400)]
         [Description("Returns a the team selections made by the user for a given game week.")]
-        public IActionResult GetPicksForEvent(int id, int eventId)
+        public async Task<IActionResult> GetPicksForEvent(int id, int eventId)
         {
             UserEventPicks picks = null;
             string errorMessage = string.Empty;
-            _userEntryService.GetUserPlayerPicksForEvent(id, eventId).Result.Subscribe(res => picks = res,
+            var result = await _userEntryService.GetUserPlayerPicksForEvent(id, eventId);
+            result.Subscribe(res => picks = res,
                 error => errorMessage = error.Message,
                 () => { });
 
@@ -86,13 +93,15 @@ namespace API.FPL.NET.Controllers
         [ProducesResponseType(typeof(User), 200)]
         [ProducesResponseType(400)]
         [Description("Returns the users current fantasy team, as well as any transfers made, and chips.")]
-        public IActionResult GetUserTeam(int id)
+        public async Task<IActionResult> GetUserTeam(int id)
         {
             UserTeam team = null;
             string errorMessage = string.Empty;
-            _userEntryService.GetUserTeam(id).Result.Subscribe(res => team = res,
+            var result = await _userEntryService.GetUserTeam(id);
+            result.Subscribe(res => team = res,
                 error => errorMessage = error.Message,
                 () => { });
+           
 
             if (!string.IsNullOrEmpty(errorMessage))
             {
@@ -106,9 +115,9 @@ namespace API.FPL.NET.Controllers
         [ProducesResponseType(typeof(List<UserTransfer>), 200)]
         [ProducesResponseType(400)]
         [Description("Returns the transfer history for a given users fantasy team.")]
-        public IActionResult GetTransfers(int id)
+        public async Task<IActionResult> GetTransfers(int id)
         {
-            (List<UserTransfer> transfers, string errorMessage) = GetUserTransfers(id);
+            (List<UserTransfer> transfers, string errorMessage) = await GetUserTransfers(id);
 
             if (transfers == null)
             {
@@ -123,9 +132,9 @@ namespace API.FPL.NET.Controllers
         [ProducesResponseType(typeof(List<UserTransfer>), 200)]
         [ProducesResponseType(400)]
         [Description("Returns the transfer history for a given users fantasy team.")]
-        public IActionResult GetTransfersForGameweek(int id, int gameweek)
+        public async Task<IActionResult> GetTransfersForGameweek(int id, int gameweek)
         {
-            (List<UserTransfer> transfers, string errorMessage) = GetUserTransfers(id, gameweek);
+            (List<UserTransfer> transfers, string errorMessage) = await GetUserTransfers(id, gameweek);
 
             if (transfers == null)
             {
@@ -144,12 +153,35 @@ namespace API.FPL.NET.Controllers
             throw new NotImplementedException("Endpoint not found, so cannot implement this yet!");
         }
 
-        private (List<UserTransfer> transfers, string errorMessage) GetUserTransfers(int id, int? gameweek = null)
+        [HttpGet("watchlist")]
+        [ProducesResponseType(typeof(UserPlayerWatchlist), 200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Description("Returns your player watchlist. You must be logged in to retrieve the data from this endpoint.")]
+        public async Task<IActionResult> GetWatchlist()
+        {
+            UserPlayerWatchlist watchlist = null;
+            string errorMessage = string.Empty;
+            var result = await _userEntryService.GetUserWatchlist();
+            result.Subscribe(res => watchlist = res,
+                error => errorMessage = error.Message,
+                () => { });
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                return BadRequest(errorMessage);
+            }
+
+            return Ok(watchlist);
+        }
+
+        private async Task<(List<UserTransfer> transfers, string errorMessage)> GetUserTransfers(int id, int? gameweek = null)
         {
             List<UserTransfer> transfers = null;
             string errorMessage = string.Empty;
 
-            _userEntryService.GetUserTransfers(id, null).Result.Subscribe(res => transfers = res,
+            var result = await _userEntryService.GetUserTransfers(id, gameweek);
+            result.Subscribe(res => transfers = res,
                 error => errorMessage = error.Message,
                 () => { });
 

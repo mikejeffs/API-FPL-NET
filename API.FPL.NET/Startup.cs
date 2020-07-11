925e78d802acc4a7e82ae2906303b67393e68878
Mike Pratt <mike.pratt@trelleborg.com>
2020-07-11 17:18:36 +0100

    Removed unused files.
    Minor cleanup.

--- API.FPL.NET/Startup.cs
+++ API.FPL.NET/Startup.cs
@@ -1,21 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
-using API.FPL.NET.Http;
-using FPL.NET.Http;
 using FPL.NET.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace API.FPL.NET
 {
